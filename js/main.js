84a2d9f3f7fc2e6d7d7979e712e8385467241999
mirayoo <66087956+mirayoo@users.noreply.github.com>
2021-11-04 13:05:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//INPUT SLIDER
+
 var swiper = new Swiper(".reviewSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -11,8 +13,44 @@ var swiper = new Swiper(".reviewSwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
   });
+
+//INPUT FILTER
+
+(function($) {
+  $.fn.inputFilter = function(inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  };
+}(jQuery));
+
+$(document).ready(function() {
+  $("#telephone").inputFilter(function(value) {
+    return /^\d*$/.test(value);
+  });
+});
+
+//COUNTDOWN
+
+var date = new Date(new Date().valueOf() + 1 * 1 * 30 * 60 * 1000);
+
+$("#countdown")
+  .countdown(date, function(event) {
+    if (event.elapsed) {
+      $(this).html(event.strftime('Акция завершена'));
+    } else {
+    $(this).html(
+      event.strftime('%M:%S')
+    );
+  }
+});
