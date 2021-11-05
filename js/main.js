7f337bdc9ea2e1647a75a47e6e75d77f60bff9d3
mirayoo <66087956+mirayoo@users.noreply.github.com>
2021-11-05 23:18:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+//HEADER ON SCROLL
+
+// Hide header on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If scrolled down and past the navbar, add class .nav-up.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+  
+    lastScrollTop = st;
+}
+
 //HEADER
 
 $(".navToggle").on("click", function(){
